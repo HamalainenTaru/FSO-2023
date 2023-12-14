f84436de675f1fdb2ae5240136505fbae2d37d9e
HamalainenTaru <thamalainen1990@gmail.com>
2023-12-14 21:17:24 +0200

    solution for part2 / task 2.6

--- part2/puhelinluettelo/src/App.jsx
+++ part2/puhelinluettelo/src/App.jsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+
 export default function App() {
-  return <h1>Hello</h1>;
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addNewPerson = (e) => {
+    e.preventDefault();
+    const newPerson = {
+      name: newName,
+    };
+    setPersons([...persons, newPerson]);
+    setNewName("");
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name:{" "}
+          <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
 }
