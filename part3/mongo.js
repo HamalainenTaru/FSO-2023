0334262f6717514fcc0611d3f6963149b8bdf0a8
HamalainenTaru <thamalainen1990@gmail.com>
2023-12-27 13:11:33 +0200

    solution for part3 / task 3.12

---
+++ part3/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+// taru
+// mongodb+srv://thamalainen1990:<password>@phonebook.uyloztx.mongodb.net/?retryWrites=true&w=majority
+
+if (process.argv.length < 3) {
+  console.log("Give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const url = `mongodb+srv://thamalainen1990:${password}@phonebook.uyloztx.mongodb.net/phoneBook?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length > 3) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then((result) => {
+    console.log(`Added ${result.name} ${result.number} to phonebook`);
+
+    mongoose.connection.close();
+  });
+}
