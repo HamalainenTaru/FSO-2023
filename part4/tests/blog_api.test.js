a98c90178fde9a74e8c35777047ca37fb12a0421
HamalainenTaru <thamalainen1990@gmail.com>
2024-01-05 18:22:50 +0200

    solution for part4 / task 4.11

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -32,6 +32,42 @@ test("Blogs have id", async () => {
   expect(blog.id).toBeDefined();
 });
 
+test("Testing POST method", async () => {
+  const newBlog = {
+    title: "My test blog",
+    author: "Thats me",
+    url: "ttps://reactpatterns.com/",
+    likes: 34,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const notesAtTheEnd = await helper.blogsInDb();
+  expect(notesAtTheEnd).toHaveLength(helper.initialBlogs.length + 1);
+});
+
+test("if likes is not given to blog, value is 0 by default", async () => {
+  const newBlog = {
+    title: "this has zero likes",
+    author: "zero likes",
+    url: "ttps://reactpatterns.com/",
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogs = await helper.blogsInDb();
+  const addedBlog = blogs.find((blog) => blog.author === "zero likes");
+  expect(addedBlog.likes === 0);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
