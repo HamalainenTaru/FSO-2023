a7a1a339044bd643c2b213d6fcace440b9b7b711
HamalainenTaru <thamalainen1990@gmail.com>
2024-01-05 13:45:12 +0200

    solution for part4 / task 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,4 +4,26 @@ const totalLikes = (blogs) => {
   return blogs.map((blog) => blog.likes).reduce((cur, next) => cur + next, 0);
 };
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce((prev, cur) =>
+    prev.likes < cur.likes ? cur : prev
+  );
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
+const mostBlogs = (blogs) => {
+  // TODO: palauta author, jolla on eniten blogeja
+  // palauta author ja blogien määrä
+};
+
+const mostLikes = (blogs) => {
+  // TODO: palauta author, jolla on eniten tykkäyksiä blogeissaan
+  // palauta author ja likejen yhteismäärä
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
