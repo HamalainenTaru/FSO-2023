3d5efbed4bb908df8ff05b9928bd1c88deb16943
HamalainenTaru <thamalainen1990@gmail.com>
2024-01-19 16:33:22 +0200

    deleted part4

--- part4/utils/config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-let MONGO_URL =
-  process.env.NODE_ENV === "test"
-    ? process.env.TEST_MONGO_URI
-    : process.env.MONGO_URI;
-let PORT = process.env.PORT;
-
-module.exports = { MONGO_URL, PORT };
